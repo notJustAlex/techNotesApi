720436589d2766a0c9a0a757ba84ea2ef912dc99
Alex <alex.zakablook@gmail.com>
2023-12-13 18:04:28 +0100

    testing to fix bug with server 0.2

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
 	"https://technotes-zvbw.onrender.com",
 	"http://localhost:3000",
+	"https://main--roaring-tulumba-0d2453.netlify.app",
 ];
 
 module.exports = allowedOrigins;
